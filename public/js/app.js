9c51516e55675d3f699ff74ae1d10d41a8c36263
JonatSrG <JonatSrG>
2022-10-23 21:04:15 -0500

    generar la suscripcion

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,7 @@
 var url = window.location.href;
 var swLocation = '/twittor/sw.js';
 
+var swReg;
 
 if ( navigator.serviceWorker ) {
 
@@ -10,8 +11,17 @@ if ( navigator.serviceWorker ) {
         swLocation = '/sw.js';
     }
 
+    window.addEventListener('load', function() {
 
-    navigator.serviceWorker.register( swLocation );
+        navigator.serviceWorker.register( swLocation ).then( function(reg) {
+                swReg = reg;
+                swReg.pushManager.getSubscription().then( verificarSuscripcion );
+        });
+
+    });
+
+
+    
 }
 
 
@@ -248,7 +258,7 @@ function verificarSuscripcion( activadas ) {
     }
 }
 
-verificarSuscripcion();
+//verificarSuscripcion();
 
 
 
@@ -293,4 +303,26 @@ function getPublicKey() {
     
 };
 
-getPublicKey().then( console.log);
+//getPublicKey().then( console.log);
+
+btnDesactivadas.on( 'click', function() {
+
+    if ( !swReg ) return console.log('No hay registro del SW');
+
+    getPublicKey().then( function( key ){
+
+        swReg.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: key
+
+        }).then( res => res.toJSON() )
+        .then( function(suscripcion) {
+            console.log(suscripcion);
+
+            verificarSuscripcion(suscripcion);
+        })
+    });
+
+    
+});
+
